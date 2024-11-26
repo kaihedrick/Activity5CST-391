6c3487fb35e494598ac7593e7c3b3cb4ecea5dfc
kaihedrick <kaihedrick@icloud.com>
2024-11-25 22:40:35 -0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+    return (
+        <div>
+            <div className="card" style={{ width: '12rem' }}>
+                <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image name" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the content.
+                    </p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
